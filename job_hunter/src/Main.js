871419ca18cd951da1485b85feb71fe2744ea431
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-18 16:49:06 -0600

    Base Pages

---
+++ job_hunter/src/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Home from "./Main";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+
+function Home() {
+    render();
+    return (
+        <HashRouter>
+          <div>
+            <h1>Simple SPA</h1>
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/stuff">Stuff</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+                <div className="content">
+                    <div className="content">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/stuff" component={Stuff}/>
+                        <Route path="/contact" component={Contact}/>
+                </div>
+            </div>
+          </div>
+        </HashRouter>
+    );
+}
+
+export default Home;
