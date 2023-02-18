00eb4eb536ef772b04b71401283553636486b9f7
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-18 16:49:25 -0600

    Home Functionality

--- job_hunter/src/App.js
+++ job_hunter/src/App.js
@@ -1,13 +1,14 @@
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./Auth"
+import Home from "./Main"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
     </BrowserRouter>
