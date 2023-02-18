b9d3c2273832be21d5436f48b66eb8f3540d04b7
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-18 15:52:19 -0600

    Adding Registration feature using useState Hook

--- job_hunter/src/Auth.js
+++ job_hunter/src/Auth.js
@@ -1,17 +1,79 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function (props) {
+  let [authMode, setAuthMode] = useState("signin")
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+
+  if (authMode === "signin") {
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Email Address"
             />
           </div>
           <div className="form-group mt-3">
@@ -19,7 +81,7 @@ export default function (props) {
             <input
               type="password"
               className="form-control mt-1"
-              placeholder="Enter password"
+              placeholder="Password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
@@ -27,7 +89,7 @@ export default function (props) {
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
+          <p className="text-center mt-2">
             Forgot <a href="#">password?</a>
           </p>
         </div>
