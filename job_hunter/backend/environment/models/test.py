dbd6e7e78d6a4cef41207cd483a294c61ed7e6a2
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-19 09:56:48 -0600

    modified test.py

--- job_hunter/backend/environment/models/test.py
+++ job_hunter/backend/environment/models/test.py
@@ -2,4 +2,10 @@ from utils import *
 csv_path = "naukri.csv"
 x_col = "Job Title"
 y_col = "Role"
-model = train_model(csv_path,x_col,y_col,"LogReg")
+
+# Train the SVM model
+svm_model = train_model(csv_path, x_col, y_col, "SVM")
+
+# Train the Multinomial Naive Bayes model
+nb_model = train_model(csv_path, x_col, y_col, "MultiNB")
+
