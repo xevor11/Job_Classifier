49d473c903aaa43ea9a33a3398e604a48df7998d
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2023-02-19 09:50:26 -0600

    JavaScript file that makes an API call to the Flask server and updates the UI with the prediction result

---
+++ job_hunter/src/connection.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const result = document.querySelector('#result');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get the job title from the form input
+  const jobTitle = input.value;
+
+  // Make an API call to the Flask server
+  fetch(`/predict/${jobTitle}`)
+    .then(response => response.json())
+    .then(data => {
+      // Update the UI with the prediction result
+      result.textContent = data;
+    })
+    .catch(error => console.error(error));
+});
