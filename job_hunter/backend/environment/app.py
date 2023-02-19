9a598f73f84c577e5af6b053f48c042b00ec3c7a
XinyuP <xinyup@umich.edu>
2023-02-19 03:15:29 -0500

    set up backend modify

--- job_hunter/backend/environment/app.py
+++ job_hunter/backend/environment/app.py
@@ -1,13 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
 import requests
 from utils import *
 from http import HTTPStatus
 import pickle
-#Defining App !
+# Defining App !
 app = Flask(__name__)
 api = Api(app)
-model:Pipeline = load_model("model_svm.pkl")
+model: Pipeline = load_model("model_svm.pkl")
 # Only one Endpoint to API supported: {GET}
 # class Model(Resource):
 #     # Endpoint to Get a Campaign already created in my datastore.
@@ -15,10 +15,11 @@ model:Pipeline = load_model("model_svm.pkl")
 #         model = load_model("model_svm.pkl")
 #         pred_industry = model.predict([job_title])
 #         return pred_industry[0],HTTPStatus.OK
-@app.route("/predict/<string:job_title>",methods = ['GET','POST'])
-def predict(job_title):
-    return model.predict([job_title])[0],HTTPStatus.OK
 
+
+@app.route("/predict/<string:job_title>", methods=['GET', 'POST'])
+def predict(job_title):
+    return model.predict([job_title])[0], HTTPStatus.OK
 
 
 # Adding routes to the Application and Endpoints to App.
